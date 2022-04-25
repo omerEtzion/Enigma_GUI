f65f75b6bac9c2f6e8397072c0bf201e73f679fd
omerEtzion <etzion.omer@gmail.com>
2022-04-25 17:06:01 +0300

    deleted port number in JavaGateway

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from py4j.java_gateway import JavaGateway, GatewayParameters
 
 if __name__ == '__main__':
 
-    gateWay = JavaGateway(25333)
+    gateWay = JavaGateway()
     enigma = gateWay.entry_point.getEnigma()
     print(enigma.getInt())
